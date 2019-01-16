5e83e08321681e1f3de84b7f90c975e6579f0fee
ShaunLWM <shaunlimweimin@gmail.com>
2019-01-16 12:38:45 +0800

    Add forceRedownload option Include resolve for results Save archiveorg results to file

--- modules/Archive.js
+++ modules/Archive.js
@@ -5,9 +5,10 @@ const puppeteer = require('puppeteer');
 const request = require('request');
 
 class Archive {
-    constructor({ dir, url }) {
+    constructor({ dir, url, forceRedownload = true }) {
         this.dirName = dir;
         this.url = url;
+        this.forceRedownload = forceRedownload;
         this.mainDirectory = path.join(__dirname, this.dirName);
         fs.ensureDirSync(this.mainDirectory);
     }
@@ -16,7 +17,7 @@ class Archive {
         return new Promise((resolve, reject) => {
             let iconPath = path.join(this.mainDirectory, 'favicon.ico');
             console.debug(`[#] fetchFavicon: ${iconPath}`);
-            if (fs.pathExistsSync(iconPath)) {
+            if (fs.pathExistsSync(iconPath) && !this.forceRedownload) {
                 console.debug(`[#] fetchFavicon: File exist.`);
                 return resolve({
                     output: 'favicon.ico',
@@ -40,28 +41,60 @@ class Archive {
     async fetchPDF() {
         let pdfPath = path.join(this.mainDirectory, 'output.pdf');
         console.debug(`[#] fetchPDF ${pdfPath}`);
+        if (fs.pathExistsSync(pdfPath) && !this.forceRedownload) {
+            console.debug(`[#] fetchPDF: File exist.`);
+            return resolve({
+                output: 'output.pdf',
+                status: 'skipped'
+            });
+        }
+
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(this.url, { waitUntil: 'networkidle2' });
         await page.pdf({ path: pdfPath, format: 'A4' });
         console.debug(`[@] fetchPDF: Downloading ${pdfPath}`);
         await browser.close();
+        return resolve({
+            output: 'output.pdf',
+            status: 'success'
+        });
     }
 
     async fetchScreenshot() {
         let screenPath = path.join(this.mainDirectory, 'output.png');
         console.debug(`[#] fetchScreenshot ${screenPath}`);
+        if (fs.pathExistsSync(screenPath) && !this.forceRedownload) {
+            console.debug(`[#] fetchScreenshot: File exist.`);
+            return resolve({
+                output: 'output.png',
+                status: 'skipped'
+            });
+        }
+
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(this.url, { waitUntil: 'networkidle2' });
         await page.screenshot({ path: screenPath, fullPage: true, omitBackground: true });
         console.debug(`[@] fetchScreenshot: Downloading ${screenPath}`);
         await browser.close();
+        return resolve({
+            output: 'output.png',
+            status: 'success'
+        });
     }
 
     async fetchDom() {
         let htmlPath = path.join(this.mainDirectory, 'output.html');
         console.debug(`[#] fetchDom ${htmlPath}`);
+        if (fs.pathExistsSync(htmlPath) && !this.forceRedownload) {
+            console.debug(`[#] fetchDom: File exist.`);
+            return resolve({
+                output: 'output.html',
+                status: 'skipped'
+            });
+        }
+
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(this.url, { waitUntil: 'networkidle2' });
@@ -69,10 +102,23 @@ class Archive {
         fs.writeFileSync(htmlPath, bodyHTML);
         console.debug(`[@] fetchDom: Downloading ${htmlPath}`);
         await browser.close();
+        return resolve({
+            output: 'output.html',
+            status: 'success'
+        });
     }
 
     async submitArchiveOrg() {
         return new Promise((resolve, reject) => {
+            let archiveFile = path.join(this.mainDirectory, 'archive.org.txt');
+            if (fs.pathExistsSync(archiveFile) && !this.forceRedownload) {
+                console.debug(`[#] submitArchiveOrg: Already submitted.`);
+                return resolve({
+                    output: fs.readFileSync(archiveFile),
+                    status: 'skipped'
+                });
+            }
+
             let domain = `http://web.archive.org`;
             let url = `${domain}/save/${this.url}`;
             console.debug(`[#] submitArchiveOrg: ${url}`);
@@ -123,6 +169,7 @@ class Archive {
                 console.debug(`[#] submitArchiveOrg: success ${url}`);
                 let archiveId = response.headers['content-location'];
                 let archiveUrl = `${domain}/${archiveId}`;
+                fs.writeFileSync(archiveFile, archiveUrl);
                 // https://github.com/pastpages/savepagenow/blob/master/savepagenow/api.py#L57
                 return resolve({
                     status: 'success',
