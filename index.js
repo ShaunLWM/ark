8755e6cdf4b7e10db3910b76ec2b80d325a69b14
ShaunLWM <shaunlimweimin@gmail.com>
2019-01-17 13:46:52 +0800

    Mount archives to express Show list of archives in index

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const fs = require('fs-extra');
+
 const ArchiveManager = require('./modules/ArchiveManager');
-let archiveManager = new ArchiveManager({ mainDirectory: path.join(__dirname, 'test', 'archives') });
-console.log(archiveManager.getArchives());
+let archivesDirectory = 'archives';
+let mainDirectory = path.join(__dirname, archivesDirectory);
+let archiveManager = new ArchiveManager({ mainDirectory });
+
 const app = express();
 let hbs = exphbs.create({
     defaultLayout: 'main.hbs',
@@ -17,6 +21,21 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 const port = 8081;
 
+fs.ensureDirSync(archivesDirectory);
 app.use(express.static('public'));
-app.get('/', (req, res) => res.render('index.hbs'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use('/archives', express.static(archivesDirectory));
+app.get('/', (req, res) => {
+    return res.render('index.hbs', {
+        archives: archiveManager.getArchives().map(archive => {
+            let dir = path.join(archivesDirectory, archive.title);
+            return {
+                favicon: (fs.pathExistsSync(path.join(dir, 'favicon.ico')) ? path.join(dir, 'favicon.ico') : ''),
+                dom: (fs.pathExistsSync(path.join(dir, 'output.html')) ? path.join(dir, 'output.html') : ''),
+                pdf: (fs.pathExistsSync(path.join(dir, 'output.pdf')) ? path.join(dir, 'output.pdf') : ''),
+                screenshot: (fs.pathExistsSync(path.join(dir, 'output.png')) ? path.join(dir, 'output.png') : ''),
+                ...archive,
+            }
+        })
+    });
+});
+app.listen(port, () => console.log(`[@] ark running on ${port}!`))
--- test/ArchiveManager.js
+++ test/ArchiveManager.js
@@ -1,5 +1,5 @@
 const path = require('path');
 // console.log(__dirname);
 const ArchiveManager = require('../modules/ArchiveManager');
-let archiveManager = new ArchiveManager({ mainDirectory: path.join(__dirname, 'archives') });
+let archiveManager = new ArchiveManager({ mainDirectory: path.join(__dirname, '..', 'archives') });
 archiveManager.addUrl('https://eatbook.sg/popular-hawker-stalls/');
